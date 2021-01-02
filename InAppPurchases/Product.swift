883f3ab3a23b5aa8a991bf9204f1d5e41308876e
mohammed <mohamed.alefrangy@gmail.com>
2021-01-02 15:30:30 +0200

    init commit

---
+++ InAppPurchases/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  InAppPurchases
+//
+//  Created by Mohammed on 1/2/21.
+//
+
+import Foundation
+import UIKit
+
+enum Product: String, CaseIterable {
+    
+    case removeAds = "com.mohammed.InAppPurchases.BuyThis"
+    case unlockEverything = "com.mohammed.InAppPurchases.BuyThis"
+    case getCoins = "com.mohammed.InAppPurchases.BuyThis"
+}
--- InAppPurchases/ViewController.swift
+++ InAppPurchases/ViewController.swift
@@ -6,14 +6,114 @@
 //
 
 import UIKit
+import StoreKit
 
-class ViewController: UIViewController {
+enum Product: String, CaseIterable {
+    
+    case removeAds = "com.mohammed.InAppPurchases.removeAds"
+    case unlockEverything = "com.mohammed.InAppPurchases.unlockEverything"
+    case getCoins = "com.mohammed.InAppPurchases.getCoins"
+}
 
+
+class ViewController: UIViewController, SKPaymentTransactionObserver, SKProductsRequestDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    private var models = [SKProduct]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initTableView()
+        fetchProudacts()
+        SKPaymentQueue.default().add(self)
+        
     }
+    
+    private func fetchProudacts(){
+        
+        let request = SKProductsRequest(productIdentifiers:
+                                            Set(Product.allCases.compactMap({ $0.rawValue } )))
+        request.delegate = self
+        request.start()
+        
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        
+        DispatchQueue.main.async {
+            print("Count: \(response.products.count)")
+            self.models = response.products
+            self.tableView.reloadData()
+        }
+    }
+    
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        transactions.forEach({
+            
+            switch $0.transactionState{
+            
+            case .purchasing:
+                print("Transaction is purchasing")
+            case .purchased:
+                print("Transaction is purchased")
+                SKPaymentQueue.default().finishTransaction($0)
+            case .failed:
+                print("Transaction is failed")
+                SKPaymentQueue.default().finishTransaction($0)
+            case .restored:
+                break
+            case .deferred:
+                break
+            @unknown default:
+                break
+                
+            }
+        })
+    }
+    
+}
 
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func initTableView(){
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let product = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = "\(product.localizedTitle): \(product.localizedDescription) -> \(product.priceLocale.currencySymbol ?? "$")\(product.price)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //show purchase
+        if SKPaymentQueue.canMakePayments(){
+            
+            let payment = SKPayment(product: models[indexPath.row])
+            SKPaymentQueue.default().add(payment)
+            
+        }else {
+            print("The user can't make transaction")
+        }
+        
+    }
+    
 }
 
